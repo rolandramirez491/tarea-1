1450120612296b59def3dfc3927e8e1430e4e9a7
rolandramirez491 <roland.ramirez491@pascualbravo.edu.co>
2024-08-27 23:57:45 -0500

    intento 1

--- app/src/main/java/com/example/tarea1/MainActivity.java
+++ app/src/main/java/com/example/tarea1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tarea1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"holaaa aqui es el oncreate");
+
+        ingresarUsuario = (EditText) findViewById(R.id.editarUsuario);
+        usuario = (TextView) findViewById(R.id.txvUsuario);
+        hola = findViewById(R.id.btnHola);
+        mundo = findViewById(R.id.btnMundo);
+
+        hola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(TAG,"boton numero uno");
+            }
+        });
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"estoy en onstar");
+
+        mundo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG,"Estoy en el onResume");
+        //holaaaa
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"Estoy en el onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"Estoy en el onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"Estoy en el onRestart");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"Estoy en el onDestroy");
     }
 }
